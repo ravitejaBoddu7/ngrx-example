a7b995bdee9018f2220418a0f56b94715e60f9b0
Raviteja Boddu <raviteja.boddu@peopletech.com>
2022-10-11 18:33:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {StoreModule} from "@ngrx/store";
-import {heroReducer} from "./heroes/reducers/hero-reducer";
 import { ReadDeleteComponent } from './heroes/components/read-delete/read-delete.component';
 import { CreateComponent } from './heroes/components/create/create.component';
+import { environment } from '../environments/environment';
+import { Store } from '@ngrx/store';
+import { first } from 'rxjs';
+import { StoreModule } from '@ngrx/store';
+import { heroReducer } from './heroes/reducers/hero-reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReadDeleteComponent,
-    CreateComponent
-  ],
+  declarations: [AppComponent, ReadDeleteComponent, CreateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(
-      { heroes: heroReducer }
-    )
+    StoreModule.forRoot({ heroes: heroReducer }),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Demo App',
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  // public constructor(private store: Store<unknown>) {
+  //   this.exposeStoreToGlobal();
+  // }
+  // public exposeStoreToGlobal() {
+  //   if (!environment.production) {
+  //     globalThis.showStoreSnapshot = () => {
+  //       this.store.pipe(first()).subscribe((snapshot) => console.log(snapshot));
+  //     };
+  //   }
+  // }
+}
