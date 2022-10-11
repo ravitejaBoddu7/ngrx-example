e967cfa9efeb532e8d048afb063510dd9e9d5cc5
Raviteja Boddu <raviteja.boddu@peopletech.com>
2022-10-11 18:40:01 +0530

    updated angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReadDeleteComponent } from './heroes/components/read-delete/read-delete.component';
 import { CreateComponent } from './heroes/components/create/create.component';
-import { environment } from '../environments/environment';
-import { Store } from '@ngrx/store';
-import { first } from 'rxjs';
 import { StoreModule } from '@ngrx/store';
 import { heroReducer } from './heroes/reducers/hero-reducer';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, ReadDeleteComponent, CreateComponent],
@@ -17,23 +13,8 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ heroes: heroReducer }),
-    StoreDevtoolsModule.instrument({
-      name: 'NgRx Demo App',
-      logOnly: environment.production,
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  // public constructor(private store: Store<unknown>) {
-  //   this.exposeStoreToGlobal();
-  // }
-  // public exposeStoreToGlobal() {
-  //   if (!environment.production) {
-  //     globalThis.showStoreSnapshot = () => {
-  //       this.store.pipe(first()).subscribe((snapshot) => console.log(snapshot));
-  //     };
-  //   }
-  // }
-}
+export class AppModule {}
